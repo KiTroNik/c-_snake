c79fbec5bcba37bddbce6ba84dd28b55e36a9d6b
Jakub <kubatom3@gmail.com>
2021-01-19 14:33:30 +0100

    added movement and random apples

--- src/snake.cpp
+++ src/snake.cpp
@@ -2,32 +2,49 @@
 
 CSnake::CSnake(CRect r, char _c /*=' '*/): CFramedWindow(r, _c)
 {
-  snake.front() = CPoint(geom.topleft.x+1, geom.topleft.y+1);
-  //previous_key = KEY_RIGHT;
+    previous_key = CPoint(1, 0);
+    snake.push_back(CPoint(geom.topleft.x+5, geom.topleft.y+5));
+    snake.push_back(CPoint(geom.topleft.x+4, geom.topleft.y+4));
+    snake.push_back(CPoint(geom.topleft.x+3, geom.topleft.y+3));
+    apple = CPoint(geom.topleft.x + 15, geom.topleft.y + 15);
 }
 
 void CSnake::paint() {
-  CFramedWindow::paint();
-  gotoyx(snake.front().y, snake.front().x);
-  printl("+");
-  napms (120 - 5 * 1); //(snake.size() / 3));
+    CFramedWindow::paint();
+    gotoyx(snake[0].y, snake[0].x);
+    printl("*");
+    for (unsigned int i = 1; i < snake.size(); i++) {
+        gotoyx(snake[i].y, snake[i].x);
+        printl("+");
+    }
+    gotoyx(apple.y, apple.x);
+    printl("o");
+    napms (120 - 5 * (snake.size() / 3));
 }
 
 bool CSnake::handleEvent(int key) {
+    if (key == '\t') {
+      return false;
+    }
+
     switch (key) {
         case KEY_UP:
+            if (previous_key.y == 1) return false;
             previous_key = CPoint(0, -1);
             move(previous_key);
             return true;
         case KEY_DOWN:
+            if (previous_key.y == -1) return false;
             previous_key = CPoint(0, 1);
             move(previous_key);
             return true;
         case KEY_LEFT:
+            if (previous_key.x == 1) return false;
             previous_key = CPoint(-1, 0);
             move(previous_key);
             return true;
         case KEY_RIGHT:
+            if (previous_key.x == -1) return false;
             previous_key = CPoint(1, 0);
             move(previous_key);
             return true;
@@ -37,17 +54,37 @@ bool CSnake::handleEvent(int key) {
     }
 }
 
-
 void CSnake::move(const CPoint & delta) {
-  snake.front() += delta;
 
-  if (snake.front().y == geom.topleft.y) {
-     snake.front() = CPoint(snake.front().x, geom.size.y + geom.topleft.y - 2);
-  } else if (snake.front().y == geom.size.y + geom.topleft.y - 1) { 
-      snake.front() = CPoint(snake.front().x, geom.topleft.y + 1);
-  } else if (snake.front().x == geom.topleft.x) {
-      snake.front() = CPoint(geom.topleft.x + geom.size.x - 2, snake.front().y);
-  } else if (snake.front().x == geom.topleft.x + geom.size.x - 1) {
-      snake.front() = CPoint(geom.topleft.x + 1, snake.front().y);
-  }
+    for (int i = snake.size(); i >= 1; i--) {
+        snake[i] = snake[i - 1];
+    }
+    snake[0] += delta;
+
+    if (snake.front().y == geom.topleft.y) {
+        snake.front() = CPoint(snake.front().x, geom.size.y + geom.topleft.y - 2);
+    } else if (snake.front().y == geom.size.y + geom.topleft.y - 1) { 
+        snake.front() = CPoint(snake.front().x, geom.topleft.y + 1);
+    } else if (snake.front().x == geom.topleft.x) {
+        snake.front() = CPoint(geom.topleft.x + geom.size.x - 2, snake.front().y);
+    } else if (snake.front().x == geom.topleft.x + geom.size.x - 1) {
+        snake.front() = CPoint(geom.topleft.x + 1, snake.front().y);
+    }
+}
+
+void CSnake::rand_apple() {
+    int is_git = 0;
+    int x, y, pos;
+    while (is_git != 1) {
+        pos = 0;
+        x = (geom.topleft.x + 1) + rand() % ((geom.topleft.x + geom.size.x - 1) - (geom.topleft.x + 1));
+        y = (geom.topleft.y + 1) + rand() % ((geom.topleft.y + geom.size.y - 1) - (geom.topleft.y + 1));
+
+        for (size_t i = 0; i < snake.size(); i++) {
+            if (snake[i].x != x && snake[i].y != y) pos++;
+        }
+
+        if (pos == snake.size()) is_git = 1;
+    }
+    apple = CPoint(x, y);
 }
--- src/snake.h
+++ src/snake.h
@@ -3,19 +3,26 @@
 
 #include "winsys.h"
 #include "screen.h"
+#include <vector>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
 class CSnake:public CFramedWindow {
   private:
-    list<CPoint> snake;
+    vector<CPoint> snake;
     CPoint apple;
     CPoint previous_key;
+    bool pause;
+    bool help;
+    bool game_over;
   public:
     CSnake(CRect r, char _c = ' ');
 
     void paint();
     bool handleEvent(int key);
     void move (const CPoint & delta);
+    void rand_apple();
 };
 
 #endif
